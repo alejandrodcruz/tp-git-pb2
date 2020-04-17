8e92de64937a7479a1ec51158447564717f31ec6
Alejandro Cruz <alcruz@alumno.unlam.edu.ar>
2020-04-17 19:48:12 -0300

    Cambio de variables

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,31 +1,39 @@
 import java.util.Scanner;
+
 public class Calculadora {
- public static void main(String[] args) {
- Integer a;
- Integer b;
- Integer opcion;
- Integer resultado;
- do{
- System.out.println("Ingrese su operación /n 1 para sumar /n 2 pararestar /n 3 para multiplicar /n 4 para dividir");
- opcion = teclado.next();
- }while(opcion < 1 && opcion > 4)
- System.out.println("Ingrese el primer numero");
- a = teclado.next();
- System.out.println("Ingrese el segundo numero");
- a = teclado.next();
- switch (opcion) {
-case opcion=1:
-	resultado = a + b;
-	break;
-case opcion=2:
-	resultado = a - b;
-	break;
-case opcion=3:
-	resultado = a * b;
-	break;
-default:
-	break;
-}
-  System.out.println("El resultado es " + resultado.toString());
- }
+	public static void main(String[] args) {
+		int a = 0;
+		int b = 0;
+		int opcion = 0;
+		int resultado = 0;
+
+		Scanner teclado = new Scanner(System.in);
+
+		do {
+
+			System.out.println(
+					"Ingrese su operación \n 1 para sumar. \n 2 pararestar. \n 3 para multiplicar. \n 4 para dividir.");
+			opcion = teclado.nextInt();
+
+		} while (opcion < 1 && opcion > 4);
+		System.out.println("Ingrese el primer numero");
+		a = teclado.nextInt();
+		System.out.println("Ingrese el segundo numero");
+		b = teclado.nextInt();
+		switch (opcion) {
+		case 1:
+			resultado = a + b;
+			break;
+
+		case 2:
+			resultado = a - b;
+			break;
+		case 3:
+			resultado = a * b;
+			break;
+		default:
+			break;
+		}
+		System.out.println("El resultado es " + resultado);
+	}
 }
